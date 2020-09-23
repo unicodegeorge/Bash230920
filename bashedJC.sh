4fc167b6f265d3f661bc76a4170c458158506332
unicodegeorge <george.dvorak@educanet.cz>
2020-09-23 15:35:18 +0200

    Testuju

--- bashedJC.sh
+++ bashedJC.sh
@@ -1,5 +1,5 @@
 git clone "https://github.com/EduTeachers/HelloWorld"
-cd C:/Shells/HelloWorld/src/com/company
+cd C:/nice/Bash230920/HelloWorld/src/com/company
 javac Main.java 2> compiler_error.txt || > compiler_out.txt
 cd ../../
 java com/company/Main 2 >> runtime_out.txt
